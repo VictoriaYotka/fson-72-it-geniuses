d130e8752316914064f5091d6bd632227c2d6dd1
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2022-12-11 15:51:27 +0200

    add js

---
+++ src/js/slick.js
@@ -0,0 +1,10 @@
+$('.one-time').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 10000,
+});
