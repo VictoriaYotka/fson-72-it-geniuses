f250b1e56671ceac2f0f30b968351c97c893b168
VictoriaYotka <115709692+VictoriaYotka@users.noreply.github.com>
2022-12-11 12:09:58 +0000

    Deploying to gh-pages from @ VictoriaYotka/fson-72-it-geniuses@aca1fc05eb536b9154f05dd60a57ce7a304893e1 🚀

---
+++ index.459dfb6a.js
@@ -0,0 +1,2 @@
+!function(){var e={headerEl:document.querySelector(".header-js")};window.onscroll=function(){window.screen.width>=768?scrollY<=70?e.headerEl.classList.remove("header-bg"):e.headerEl.classList.add("header-bg"):scrollY<=120?e.headerEl.classList.remove("header-bg"):e.headerEl.classList.add("header-bg")}}();
+//# sourceMappingURL=index.459dfb6a.js.map
---
+++ index.894b8646.js
@@ -0,0 +1,2 @@
+const e={headerEl:document.querySelector(".header-js")};window.onscroll=function(){window.screen.width>=768?scrollY<=70?e.headerEl.classList.remove("header-bg"):e.headerEl.classList.add("header-bg"):scrollY<=120?e.headerEl.classList.remove("header-bg"):e.headerEl.classList.add("header-bg")};
+//# sourceMappingURL=index.894b8646.js.map
